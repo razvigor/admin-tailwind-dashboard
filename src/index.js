09e46cc19c3cbeb192e4250bdc9b589e6c62f282
razvigor <kamaradsasa@gmail.com>
2022-11-14 11:35:12 +0100

    components, pages, context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './contexts/ContextProvider';
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense(
+	'ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0RiWn5cdH1VQmVeUUw='
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ContextProvider>
+			<App />
+		</ContextProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Employees.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Empoyees = () => {
+	return <div>Empoyees</div>;
+};
+
+export default Empoyees;
