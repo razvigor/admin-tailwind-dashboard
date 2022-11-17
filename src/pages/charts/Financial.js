10efd72fc1339fe15d424d1d47c92626be568e03
razvigor <kamaradsasa@gmail.com>
2022-11-17 14:15:25 +0100

    financial

--- src/pages/charts/Financial.js
+++ src/pages/charts/Financial.js
@@ -24,7 +24,7 @@ const date1 = new Date('2017, 1, 1');
 
 function filterValue(value) {
 	if (value.x >= date1) {
-		return value.x, value.high, value.low;
+		return value;
 	}
 }
 const returnValue = financialChartData.filter(filterValue);
